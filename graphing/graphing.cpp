94efceda4e6fd5340b6401d4ea08afbaa4696f47
Greg Jensen <77466937+g-jensen@users.noreply.github.com>
2021-10-30 17:19:05 -0400

    moved globals to separate file

--- graphing/graphing.cpp
+++ graphing/graphing.cpp
@@ -1,8 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include "Camera.h"
-#include "Camera.cpp"
 #include <iostream>
+#include "Globals.h"
 
 
 //sf::RectangleShape linear(float slope, float yintercept) {
@@ -15,23 +15,23 @@ std::vector<sf::CircleShape> quadratic() {
 
     std::vector<sf::CircleShape> output;
     for (float x = 0; x <= 600; x += 0.5) {
-        sf::CircleShape circle(globals::camera.zoomScale * 2);
-        circle.setPosition(x,(globals::window.getSize().y - (1.0f/20.0f * ((x-300.f)*(x-300.f))+200.f)));
+        sf::CircleShape circle(Globals::camera.zoomScale * 2);
+        circle.setPosition(x,(Globals::window.getSize().y - (1.0f/20.0f * ((x-300.f)*(x-300.f))+200.f)));
         output.push_back(circle);
     }
     return output;
 }
 
 sf::RectangleShape xaxis() {
-    sf::View view = globals::window.getView();
-    sf::RectangleShape output(sf::Vector2f(view.getSize().x, globals::camera.zoomScale * 5));
+    sf::View view = Globals::window.getView();
+    sf::RectangleShape output(sf::Vector2f(view.getSize().x, Globals::camera.zoomScale * 5));
     output.setPosition(view.getCenter().x - (view.getSize().x / 2.0f), 0);
     return output;
 }
 
 sf::RectangleShape yaxis() {
-    sf::View view = globals::window.getView();
-    sf::RectangleShape output(sf::Vector2f(view.getSize().y, globals::camera.zoomScale * 5));
+    sf::View view = Globals::window.getView();
+    sf::RectangleShape output(sf::Vector2f(view.getSize().y, Globals::camera.zoomScale * 5));
     output.setPosition(0,view.getCenter().y + (view.getSize().y / 2.0));
     output.rotate(-90);
     return output;
@@ -40,59 +40,59 @@ sf::RectangleShape yaxis() {
 int main()
 {
     // run the program as long as the window is open
-    while (globals::window.isOpen())
+    while (Globals::window.isOpen())
     {
-        globals::window.setFramerateLimit(100);
+        Globals::window.setFramerateLimit(100);
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
-        while (globals::window.pollEvent(event))
+        while (Globals::window.pollEvent(event))
         {
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed) {
-                globals::window.close();
+                Globals::window.close();
             }
             if (event.type == sf::Event::MouseWheelMoved) {
                 if (event.mouseWheel.delta >= 1) {
-                    globals::camera.zoom(0.9);
+                    Globals::camera.zoom(0.9);
                 }
                 else if (event.mouseWheel.delta <= -1) {
-                    globals::camera.zoom(1.1);
+                    Globals::camera.zoom(1.1);
                 }
             }
             if (event.type == sf::Event::KeyPressed) {
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-                    globals::camera.move(globals::camera.zoomScale * -globals::camera.moveSpeed,0);
+                    Globals::camera.move(Globals::camera.zoomScale * -Globals::camera.moveSpeed,0);
                 }
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-                    globals::camera.move(globals::camera.zoomScale * globals::camera.moveSpeed, 0);
+                    Globals::camera.move(Globals::camera.zoomScale * Globals::camera.moveSpeed, 0);
                 }
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-                    globals::camera.move(0, globals::camera.zoomScale * -globals::camera.moveSpeed);
+                    Globals::camera.move(0, Globals::camera.zoomScale * -Globals::camera.moveSpeed);
                 }
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-                    globals::camera.move(0, globals::camera.zoomScale * globals::camera.moveSpeed);
+                    Globals::camera.move(0, Globals::camera.zoomScale * Globals::camera.moveSpeed);
                 }
             }
         }
 
-        std::cout << globals::window.mapPixelToCoords(sf::Mouse::getPosition(globals::window)).x << ", " << globals::window.mapPixelToCoords(sf::Mouse::getPosition(globals::window)).y << std::endl;
+        std::cout << Globals::window.mapPixelToCoords(sf::Mouse::getPosition(Globals::window)).x << ", " << Globals::window.mapPixelToCoords(sf::Mouse::getPosition(Globals::window)).y << std::endl;
         // std::cout << camera.zoomScale << std::endl;
 
         // clear the window with black color
-        globals::window.clear(sf::Color::Black);
+        Globals::window.clear(sf::Color::Black);
 
         // draw everything here...
-        globals::window.draw(xaxis());
-        globals::window.draw(yaxis());
+        Globals::window.draw(xaxis());
+        Globals::window.draw(yaxis());
 
         for (auto i : quadratic()) {
-            globals::window.draw(i);
+            Globals::window.draw(i);
         }
 
         // end the current frame
-        globals::window.display();
+        Globals::window.display();
 
-        globals::window.setView(globals::camera.view);
+        Globals::window.setView(Globals::camera.view);
     }
 
     return 0;
