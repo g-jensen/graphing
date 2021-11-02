76b80a4c83e40d0fe9069dfc8c824d75d27c945e
Greg Jensen <77466937+g-jensen@users.noreply.github.com>
2021-11-01 21:35:45 -0400

    x-axis number line good enough for now

--- graphing/Globals.h
+++ graphing/Globals.h
@@ -6,10 +6,27 @@
 #ifndef GLOBALS
 #define GLOBALS
 
-namespace Globals {
-	sf::ContextSettings settings(0,0,8);
-	sf::RenderWindow window(sf::VideoMode(1520, 980), "My window", sf::Style::Default,settings);
-	Camera camera(window);
-}
+class Globals {
+public:
+	static sf::RenderWindow* window;
+	static sf::ContextSettings settings;
+	static Camera camera;
+	// static sf::Font font;
+	// static bool fontHasBeenLoaded;
+	// static void loadFont(std::string path);
+};
+
+//void Globals::loadFont(std::string path) {
+//	if (!Globals::fontHasBeenLoaded) {
+//		Globals::font.loadFromFile(path);
+//		Globals::fontHasBeenLoaded = true;
+//	}
+//}
+
+// bool Globals::fontHasBeenLoaded = false;
+sf::ContextSettings Globals::settings = sf::ContextSettings(0,0,8);
+sf::RenderWindow* Globals::window = new sf::RenderWindow(sf::VideoMode(1520, 980), "My window", sf::Style::Default,Globals::settings);
+Camera Globals::camera(*Globals::window);
+
 
 #endif
